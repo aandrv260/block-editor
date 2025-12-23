9b8c2b6b72ad206fdcdccb4466ef77b757f56504
Angel <92414773+aandrv260@users.noreply.github.com>
2025-12-23 12:07:43 +0200

    fix: eslint linting files in dist folder

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,13 @@ import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
 export default tseslint.config([
-  globalIgnores(["dist", "coverage", "node_modules", "**/build/**"]),
+  globalIgnores([
+    "**/dist",
+    "**/coverage",
+    "node_modules",
+    "**/node_modules",
+    "**/build",
+  ]),
 
   // Base TypeScript rules (ALL packages)
   {
