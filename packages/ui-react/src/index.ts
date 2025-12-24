5be43ca5e4855a42a3d7d71f28dd5ca63bca4df1
Angel <92414773+aandrv260@users.noreply.github.com>
2025-12-24 10:26:47 +0200

    feat(ui-react): add missing exports

--- packages/ui-react/src/index.ts
+++ packages/ui-react/src/index.ts
@@ -1,3 +1,5 @@
 // TODO: Consider before the release of a stable version 1.0.0 to use a more specific export strategy. For example, allow the user to import only the parts of the core they need like `import { EditorDocument } from "@block-editor/core/document";`.
+export * from "./components";
 export * from "./context";
 export * from "./models";
+export * from "./hooks";
