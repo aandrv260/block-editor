7d347bee37c15c25e6d9914082111a4d4f5b51a8
Angel <92414773+aandrv260@users.noreply.github.com>
2025-12-31 10:46:15 +0200

    fix(ui-react): only one block at a time can have empty text placeholder now

--- packages/ui-react/src/hooks/blocks/useBlockIsEmptyState.ts
+++ packages/ui-react/src/hooks/blocks/useBlockIsEmptyState.ts
@@ -1,12 +1,38 @@
+import { isElementFocused } from "@/common/dom-state/focus.utils";
 import type { DeepReadonly, HeadingBlock } from "@block-editor/core";
 import { useCallback, useState } from "react";
+import { useEditor } from "../useEditor";
 
-export const useBlockIsEmptyState = (block: DeepReadonly<HeadingBlock>) => {
+export const useBlockIsEmptyState = (
+  block: DeepReadonly<HeadingBlock>,
+  headingRef: React.RefObject<HTMLHeadingElement | null>,
+) => {
   const [showEmptyText, setShowEmptyText] = useState(block.data.text === "");
+  const { editor } = useEditor();
 
   const updateIsEmptyText = useCallback((newValue: string) => {
     setShowEmptyText(newValue === "");
   }, []);
 
-  return { showEmptyText, updateIsEmptyText };
+  const showEmptyTextOnFocus = () => {
+    setShowEmptyText(
+      headingRef.current?.innerText === "" && isElementFocused(headingRef.current),
+    );
+  };
+
+  const removeEmptyTextOnBlur = () => {
+    const isOnlyBlockInDocument =
+      editor.getDocumentSize() === 2 && editor.getBlock(block.id) === block;
+
+    if (isOnlyBlockInDocument) return;
+
+    setShowEmptyText(false);
+  };
+
+  return {
+    showEmptyText,
+    updateIsEmptyText,
+    showEmptyTextOnFocus,
+    removeEmptyTextOnBlur,
+  };
 };
